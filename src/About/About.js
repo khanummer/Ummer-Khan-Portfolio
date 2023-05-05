53e52c7aba35d22a9c70bcaf50542ed053f2eafc
Ummer Khan <khanummergit@gmail.com>
2023-05-05 15:50:55 -0700

    adds headshot img and edits css grid of About component

--- src/About/About.js
+++ src/About/About.js
@@ -5,18 +5,21 @@ class About extends Component {
   render() {
     return (
     <div className="about">
-        <div className="about-title">About Me</div>
+        <div className="about-title">ABOUT</div>
 
         <div className="about-text">
-            HEY THERE, I'M UMMER. A 25 YEAR OLD FULL-STACK WEB DEVELOPER WHO IS HUNGRY TO GROW IN THIS INDUSTRY.
+            HEY THERE, I'M UMMER.<br></br>
+             A 25 YEAR OLD FULL-STACK WEB DEVELOPER WHO IS HUNGRY TO GROW IN THIS INDUSTRY.
          </div>
 
         <div className="about-skills">
-            TECHNICAL SKILLS 
+            TECHNICAL SKILLS <br></br>
             HTML | CSS | JAVASCRIPT | NODE | EXPRESS | REACT | PYTHON | BOOTSTRAP | JQUERY | API'S 
             | GIT | GITHUB | POSTMAN | VSCODE | VIRTUAL MACHINES | MONGODB | MONGOOSE | FLASK | GRAPHQL 
             | FLEXBOX | CSS GRID | SQL 
         </div>
+
+        <img className="about-pic" src="../../imgs/headshot.jpg"></img>
 
     </div>
     );
